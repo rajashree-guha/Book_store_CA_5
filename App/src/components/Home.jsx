dc24c3cc3f97dd0c82be7bc7eff298131e5d5895
rajashree-guha <rajashree.625.guha@gmail.com>
2024-01-05 22:59:23 +0530

    done

--- App/src/components/Home.jsx
+++ App/src/components/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
     console.log(e);
   };
 
-// fetching data from api 
+  // fetching data from api 
   useEffect(() => {
     axios.get("https://reactnd-books-api.udacity.com/books", { headers: { 'Authorization': 'whatever-you-want' } })
       .then((res) => {
@@ -26,29 +26,35 @@ const Home = () => {
   }, []);
 
   // filtering the books on searching 
-  const searchBooks =books.filter(book=>{
+  const searchBooks = books.filter(book => {
     return book.title.toLowerCase().startsWith(search.toLowerCase())
-  })
+  });
 
   // html 
   return (
     <>
       <Navbar searchText={handleSearch} />
       <div className='grid' style={{ marginTop: "50px", position: "absolute", width: "100%", top: "15%", zIndex: "-1" }} >
-        {searchBooks.map((book) => (
-          <div id="searchBookDiv" style={{ width: "300px", textAlign: "center", padding: "30px 10px", border: "3px solid #ef3737f3", borderRadius: "10px", boxShadow: "2px 2px 2px 2px  rgba(0, 0, 0, 0.363)" }}>
-            <div>
-              <div key={book.id}>
-                <img style={{ width: "250px", height: "300px", border: "1px solid black" }} src={book.imageLinks.thumbnail} alt="" />
-                <h4 style={{ width: "250px", fontSize: "18px", margin: "10px 0" }}>{book.title}</h4>
-                <div className='flex' style={{ justifyContent: "center", color: "grey" }}>
-                  <h5 style={{ padding: "0 5px" }}>{book.averageRating ? book.averageRating+"⭐": "--⭐"}</h5>
-                  <h5>Free</h5>
+        {searchBooks.length > 0 ? (
+          searchBooks.map((book) => (
+            <div id="searchBookDiv" style={{ width: "300px", textAlign: "center", padding: "30px 10px", border: "3px solid #ef3737f3", borderRadius: "10px", boxShadow: "2px 2px 2px 2px  rgba(0, 0, 0, 0.363)" }}>
+              <div>
+                <div key={book.id}>
+                  <img style={{ width: "250px", height: "300px", border: "1px solid black" }} src={book.imageLinks.thumbnail} alt="" />
+                  <h4 style={{ width: "250px", fontSize: "18px", margin: "10px 0" }}>{book.title}</h4>
+                  <div className='flex' style={{ justifyContent: "center", color: "grey" }}>
+                    <h5 style={{ padding: "0 5px" }}>{book.averageRating ? book.averageRating + "⭐" : "--⭐"}</h5>
+                    <h5>Free</h5>
+                  </div>
                 </div>
               </div>
             </div>
+          ))
+        ) : (
+          <div style={{display:"flex",alignItems:"center",justifyContent:"center",width:"100vw"}}>
+            <img src="https://media0.giphy.com/media/8L0Pky6C83SzkzU55a/giphy.gif" alt="No books found" style={{ display: "block", marginTop: "20px" }} />
           </div>
-        ))}
+        )}
       </div>
     </>
   );
--- App/src/components/NavbarForm.jsx
+++ App/src/components/NavbarForm.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className='flex nav justify align'>
-        <div><h2 className='h2'style={{color:"white",fontFamily:"'Nova Square', sans-serif", fontSize:"30px",fontSize:"30px"}}>Kalvium Books</h2></div>
+        <div><h2 className='h2'style={{color:"white",fontFamily:"'Nova Square', sans-serif",fontSize:"30px"}}>Kalvium Books</h2></div>
         {/* Linking button to home page   */}
         <div> <Link to={"/"}>
             <button id='btn'>H O M E</button>
